39628f066acad570f84a04f7d5a7e230088a5c17
Your Name <you@example.com>
2025-11-04 12:17:30 +0300

    u

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -28,6 +28,7 @@ static void my_application_activate(GApplication* application) {
   GtkWidget* window_widget = GTK_WIDGET(window);
   gtk_widget_set_app_paintable(window_widget, TRUE);
   GdkScreen* screen = gtk_window_get_screen(window);
+
 #if GTK_CHECK_VERSION(3, 0, 0)
   GdkVisual* visual = gdk_screen_get_rgba_visual(screen);
   if (visual != nullptr) {
@@ -35,14 +36,26 @@ static void my_application_activate(GApplication* application) {
   }
 #endif
 
+  // === (بدء الكود لجعل الخلفية بلور) ===
+  {
+    GtkCssProvider* blur_provider = gtk_css_provider_new();
+    const gchar* blur_css =
+        "window {"
+        "  background-color: rgba(0, 0, 0, 0.25);"
+        "  backdrop-filter: blur(25px);"
+        "  -gtk-backdrop-filter: blur(25px);"
+        "}";
 
-  // Use a header bar when running in GNOME as this is the common style used
-  // by applications and is the setup most users will be using (e.g. Ubuntu
-  // desktop).
-  // If running on X and not using GNOME then just use a traditional title bar
-  // in case the window manager does more exotic layout, e.g. tiling.
-  // If running on Wayland assume the header bar will work (may need changing
-  // if future cases occur).
+    gtk_css_provider_load_from_data(blur_provider, blur_css, -1, NULL);
+    gtk_style_context_add_provider_for_screen(
+        gdk_screen_get_default(),
+        GTK_STYLE_PROVIDER(blur_provider),
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref(blur_provider);
+  }
+  // === (نهاية الكود) ===
+
+  // Header bar setup
   gboolean use_header_bar = TRUE;
 #ifdef GDK_WINDOWING_X11
   if (GDK_IS_X11_SCREEN(screen)) {
@@ -55,18 +68,13 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
 
-    // --- (بداية الكود المضاف لتغيير اللون) ---
     GtkCssProvider* provider = gtk_css_provider_new();
-    
-    // اللون الذي طلبته: A=162 (162/255 = 0.635), R=0, G=0, B=0
-    // نستخدم "headerbar" كـ "selector" لاستهداف الـ GtkHeaderBar
     const gchar* css = "headerbar { background-color: rgba(0, 0, 0, 0.635); }";
-    
     gtk_css_provider_load_from_data(provider, css, -1, NULL);
     GtkStyleContext* context = gtk_widget_get_style_context(GTK_WIDGET(header_bar));
-    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider),
+                                   GTK_STYLE_PROVIDER_PRIORITY_USER);
     g_object_unref(provider);
-    // --- (نهاية الكود المضاف) ---
 
     gtk_widget_show(GTK_WIDGET(header_bar));
     gtk_header_bar_set_title(header_bar, "vafile");
@@ -83,29 +91,22 @@ static void my_application_activate(GApplication* application) {
 
   FlView* view = fl_view_new(project);
   GdkRGBA background_color;
-  // Background defaults to black, override it here if necessary, e.g. #00000000 for transparent.
-    gdk_rgba_parse(&background_color, "#00000000"); // transparent
-    fl_view_set_background_color(view, &background_color);
+  gdk_rgba_parse(&background_color, "#00000000"); // شفافية كاملة
+  fl_view_set_background_color(view, &background_color);
   gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
   // Show the window when Flutter renders.
-  // Requires the view to be realized so we can start rendering.
   g_signal_connect_swapped(view, "first-frame", G_CALLBACK(first_frame_cb), self);
   gtk_widget_realize(GTK_WIDGET(view));
 
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
-
   gtk_widget_grab_focus(GTK_WIDGET(view));
-
-
 }
-
 
 // Implements GApplication::local_command_line.
 static gboolean my_application_local_command_line(GApplication* application, gchar*** arguments, int* exit_status) {
   MyApplication* self = MY_APPLICATION(application);
-  // Strip out the first argument as it is the binary name.
   self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
 
   g_autoptr(GError) error = nullptr;
@@ -117,25 +118,16 @@ static gboolean my_application_local_command_line(GApplication* application, gch
 
   g_application_activate(application);
   *exit_status = 0;
-
   return TRUE;
 }
 
 // Implements GApplication::startup.
 static void my_application_startup(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application startup.
-
   G_APPLICATION_CLASS(my_application_parent_class)->startup(application);
 }
 
 // Implements GApplication::shutdown.
 static void my_application_shutdown(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application shutdown.
-
   G_APPLICATION_CLASS(my_application_parent_class)->shutdown(application);
 }
 
@@ -157,10 +149,6 @@ static void my_application_class_init(MyApplicationClass* klass) {
 static void my_application_init(MyApplication* self) {}
 
 MyApplication* my_application_new() {
-  // Set the program name to the application ID, which helps various systems
-  // like GTK and desktop environments map this running application to its
-  // corresponding .desktop file. This ensures better integration by allowing
-  // the application to be recognized beyond its binary name.
   g_set_prgname(APPLICATION_ID);
 
   return MY_APPLICATION(g_object_new(my_application_get_type(),
